24286700e9dac3193f2990065820749025fa4cd6
srushti-rana <Sruhstirana3001@gmail.com>
2023-03-02 16:57:54 +0530

    new file upload3

---
+++ Controllers/ListingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RealEstate.Controllers
+{
+    public class ListingController : Controller
+    {
+        public IActionResult Listing()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/aboutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RealEstate.Controllers
+{
+    public class aboutController : Controller
+    {
+        public IActionResult about()
+        {
+            return View();
+        }
+    }
+}
